Fixed all numerical errors.

SimpleNN/test_nn_framework/base_engine.py
@@ -15,8 +15,8 @@ class BaseEngine:
         self.LAYER_FILENAME = "layer_mnist_{0}.p"
         self.testbatch_size = 128
         self.minibatch_size = 128
-        self.epochs = 1
-        self.batch_limit = 2
+        self.epochs = 10
+        self.batch_limit = 100
 
         self.layer_height = [28 * 28, 512, 256, 10] # MNIST
         # self.layer_height = [32 * 32 * 3, 1024, 512, 512, 10]  # CIFAR-10
@@ -24,7 +24,7 @@ class BaseEngine:
         self.layers = len(self.layer_height) - 1
         self.hidden_layers = len(self.layer_height) - 2
 
-        self.learn_rate = 0.01
+        self.learn_rate = 0.001
         self.regulation_strength = 0.002
 
         self.act = {}
@@ -42,9 +42,8 @@ class BaseEngine:
             num_classes = 2
 
             self.LAYER_FILENAME = "verify_{0}x{0}_{{0}}.p".format(section_size)
-            self.testbatch_size = 5
-            self.minibatch_size = 5
-            self.epochs = 1
+            self.testbatch_size = 16
+            self.minibatch_size = 16
 
             self.layer_height = [section_size**2, 4, 2]  # verify sizes
 
@@ -61,7 +60,7 @@ class BaseEngine:
             
             # the data, split between train and test sets
             (self.x_train, self.y_train), (self.x_test, self.y_test) = input_data.load_data()
-
+          
             midpoint = self.x_train.shape[1]/2
             start = int(midpoint-section_size/2)
             end = int(start+section_size)
@@ -75,7 +74,12 @@ class BaseEngine:
             test_filter = np.where((self.y_test < num_classes))
 
             self.x_train, self.y_train = self.x_train[train_filter], self.y_train[train_filter]
-            self.x_test, self.y_test = self.x_test[test_filter], self.y_test[test_filter]
+            self.x_test, self.y_test = self.x_test[test_filter], self.y_test[test_filter]     
+
+            # self.x_train = self.x_train[:5,:,:]
+            # self.y_train = self.y_train[:5]
+            # self.x_test = self.x_test[:5,:,:]
+            # self.y_test = self.y_test[:5]      
 
         else:
             print("Loading data...")
@@ -83,16 +87,16 @@ class BaseEngine:
 
         self.y_train = self.y_train.reshape((self.y_train.shape[0],))
         self.x_train = self.x_train.reshape(self.x_train.shape[0], self.layer_height[0])
-        self.x_train = self.x_train.astype('float32')
+        self.x_train = self.x_train.astype(settings.NN_T)
         self.x_train /= 255
 
         self.y_test = self.y_test.reshape((self.y_test.shape[0],))
         self.x_test = self.x_test.reshape(self.x_test.shape[0], self.layer_height[0])
-        self.x_test = self.x_test.astype('float32')
+        self.x_test = self.x_test.astype(settings.NN_T)
         self.x_test /= 255
 
-        self.y_train = keras.utils.to_categorical(self.y_train, self.layer_height[self.layers]).astype('float32')
-        self.y_test = keras.utils.to_categorical(self.y_test, self.layer_height[self.layers]).astype('float32')
+        self.y_train = keras.utils.to_categorical(self.y_train, self.layer_height[self.layers]).astype(settings.NN_T)
+        self.y_test = keras.utils.to_categorical(self.y_test, self.layer_height[self.layers]).astype(settings.NN_T)
 
         self.correct_pred = 0
         self.wrong_pred = 0

SimpleNN/test_nn_framework/cpu_engine.py
@@ -106,12 +106,12 @@ class CPUEngine(BaseEngine):
             batches = self.batch_limit
         #batches = 20
         print("Training with {} batches of size {}".format(batches, self.minibatch_size))
+
         for epoch in range(0, self.epochs):
             print("Epoch {} of {}...".format(epoch+1, self.epochs))
             #self.shuffle_train_set()
             for batch in range(0, batches):
                 self.set_train_input(batch)
-                #print("input",self.act[0][0])
 
                 self.fw_function()
                 #print("after fw act2 nz", np.count_nonzero(self.act[2]))

SimpleNN/test_nn_framework/fpga_engine.py
@@ -72,7 +72,7 @@ class FPGAEngine(BaseEngine):
         self.kForwardSoftMax.set_scalar_arg_dtypes([None, np.int32, np.int32])
         self.kBackwardFirstDelta.set_scalar_arg_dtypes([None, None, None, np.int32, np.int32])
         self.kBackward.set_scalar_arg_dtypes(
-            [None, None, None, None, None, None, settings.NN_T, np.int32, np.int32, np.int32])
+            [None, None, None, None, None, None, settings.NN_T, np.int32, np.int32, np.int32, np.int32])
 
         self.queue = cl.CommandQueue(self.ctx)
 
@@ -115,23 +115,23 @@ class FPGAEngine(BaseEngine):
             rows_in = self.layer_height[layer]
             is_last_layer = layer == self.layers
             if is_last_layer:
-                print("Running layer {} first_delta kernel".format(layer))
+                #print("Running layer {} first_delta kernel".format(layer))
                 self.kBackwardFirstDelta.set_args(self.act_buffers[layer], self.ground_truth_buffer,
                                                   self.delta_buffers[layer], self.minibatch_size,
                                                   self.layer_height[layer])
                 cl.enqueue_task(self.queue, self.kBackwardFirstDelta).wait()
-                print("Running layer {} first_delta kernel. COMPLETED".format(layer))
+                #print("Running layer {} first_delta kernel. COMPLETED".format(layer))
             else:
                 rows_out = self.layer_height[layer + 1]
 
-                print("Running layer {} backwards kernel".format(layer))
+                #print("Running layer {} backwards kernel".format(layer))
                 self.kBackward.set_args(self.act_buffers[layer], self.weights_buffers[layer], self.dW_buffers[layer],
                                         self.bias_buffers[layer], self.delta_buffers[layer],
                                         self.delta_buffers[layer + 1],
-                                        self.learn_rate, self.minibatch_size, rows_in, rows_out)
+                                        self.learn_rate, self.minibatch_size, rows_in, rows_out, layer)
                 cl.enqueue_task(self.queue, self.kBackward).wait()
 
-                print("Running layer {} backwards kernel. COMPLETED".format(layer))
+                #print("Running layer {} backwards kernel. COMPLETED".format(layer))
 
     def train(self):
         batches = int(self.x_train.shape[0] / self.minibatch_size)
@@ -142,10 +142,10 @@ class FPGAEngine(BaseEngine):
 
         for epoch in range(0, self.epochs):
             print("Epoch {} of {}...".format(epoch + 1, self.epochs))
-            self.shuffle_train_set()
+            #self.shuffle_train_set()
             for batch in range(0, batches):
                 self.set_train_input(batch)
-
+                
                 self.fw_function()
 
                 self.bw_function()
@@ -189,12 +189,10 @@ class FPGAEngine(BaseEngine):
 
     def set_train_input(self, batch):
         super(FPGAEngine, self).set_train_input(batch)
-
         cl.enqueue_copy(self.queue, self.act_buffers[0], self.act[0]).wait()
         cl.enqueue_copy(self.queue, self.ground_truth_buffer, self.ground_truth).wait()
 
     def set_test_input(self, batch):
-
         super(FPGAEngine, self).set_test_input(batch)
         cl.enqueue_copy(self.queue, self.act_buffers[0], self.act[0]).wait()
         cl.enqueue_copy(self.queue, self.ground_truth_buffer, self.ground_truth).wait()

SimpleNN/test_nn_framework/settings.py
@@ -3,11 +3,12 @@ import numpy as np
 from sys import platform
 
 if platform == "darwin":
-    VENDOR_NAME = "oclgrind"
+    VENDOR_NAME = "apple"
 elif platform == "win32":
     VENDOR_NAME = "nvidia"
 else:
     VENDOR_NAME = "intel"
-DEVICE_TYPE = cl.device_type.GPU if VENDOR_NAME == 'apple' or VENDOR_NAME == 'nvidia' or VENDOR_NAME == 'oclgrind' else cl.device_type.ACCELERATOR
-print(DEVICE_TYPE, cl.device_type.GPU)
+
+DEVICE_TYPE = cl.device_type.GPU if VENDOR_NAME == 'apple' or VENDOR_NAME == 'nvidia' or VENDOR_NAME == 'amd'  or VENDOR_NAME == 'oclgrind' else cl.device_type.ACCELERATOR
+
 NN_T = np.float32